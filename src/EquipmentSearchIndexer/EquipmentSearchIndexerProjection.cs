8406d7f60e00d65b4edd90235b58f1836adca695
Rune Nielsen <runenielsen@runbox.com>
2022-03-24 16:51:35 +0100

    removes unused namespace

--- src/EquipmentSearchIndexer/EquipmentSearchIndexerProjection.cs
+++ src/EquipmentSearchIndexer/EquipmentSearchIndexerProjection.cs
@@ -5,7 +5,6 @@ using OpenFTTH.EventSourcing;
 using OpenFTTH.UtilityGraphService.Business.TerminalEquipments.Events;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Typesense;
 
